7d65c2398dfcf3a8a78961a26bc0fc21e6012b24
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2023-05-11 02:59:33 +0300

    made buttons without icons yet

---
+++ src/shared/components/CircleButton/CircleButton.styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const CircleBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 80px;
+  height: 80px;
+  padding: 10px;
+
+  font-family: 'Manrope';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 1.33;
+  color: #ffffff;
+
+  background: linear-gradient(#54adff, #54adff);
+  border-radius: 50px;
+  border: none;
+  outline: none;
+  box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
+
+  transition: background 250ms ease;
+
+  &:hover {
+    background: linear-gradient(290.46deg, #419ef1 0%, #9bd0ff 107.89%);
+  }
+`;
