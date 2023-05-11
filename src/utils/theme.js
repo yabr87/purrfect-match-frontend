b1c2d63c3471082670638c33bfe836645550ce7c
beligrous <beligrous@gmail.com>
2023-05-11 10:08:12 +0300

    fonts

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -7,8 +7,26 @@ const theme = {
     yellow: "#FFC107",
     activeButton: "#54ADFF",
     cardTopItems: "#CCE4FB",
-  }
+  },
   font: {
+    title: "Manrope",
 
-  };
-}:
+  },
+  fontWeith: {
+    title: 700,
+    header: 500,
+    
+  },
+  textSize: {
+    title: 48,
+    button: 16,
+  },
+};
+
+
+
+
+Weight
+700
+Size
+48px
