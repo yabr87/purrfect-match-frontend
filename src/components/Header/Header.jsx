8d936b365b21d8e07f78bdbf7cbb6e3c35034f2f
Anna Bramarova <annabramarova@gmail.com>
2023-05-11 22:27:33 +0300

    fix: final header logic, useMemo()

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,58 +1,56 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { useMedia } from 'shared/hooks/useMedia';
 
-import { AppHeader, HeaderContainer, MobileAuth, MobileContainer, OpenLinksButton } from './Header.styles';
+import {
+  AppHeader,
+  HeaderContainer,
+  MobileAuth,
+  MobileContainer,
+  OpenLinksButton,
+} from './Header.styles';
 import Logo from './Logo';
 import Container from 'shared/components/Container';
 import AuthNav from './AuthNav';
 import Nav from './Nav';
-// import UserNav from './UserNav';
-
 
 const Header = () => {
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
-  // const { isLoggedIn } = useAuth();
-
   const screenSize = useMedia(
     ['(min-width: 1280px)', '(min-width: 768px)', '(min-width: 480px)', '(max-width: 480px)'],
-    ['xl', 'lg', 'md', 'sm'],
+    ['desktop', 'tablet', 'mobile', 'small'],
     'xs'
   );
- 
+  const authNav = useMemo(() => <AuthNav />, []);
+  const nav = useMemo(() => <Nav />, []);
+
   return (
     <AppHeader>
       <Container>
-        <HeaderContainer >
+        <HeaderContainer>
           <Logo />
-          {screenSize === 'xl' && (
+          {screenSize === 'desktop' && (
             <>
-              <Nav />
-              <AuthNav />
-              {/* {!isLoggedIn ? <AuthNav /> : <UserNav />} */}
+              {nav}
+              {authNav}
             </>
           )}
-          {screenSize === 'lg'&& !isMobileNavOpen && <AuthNav />}
-          {screenSize !== 'xl' && (
+          {screenSize === 'tablet' && !isMobileNavOpen && authNav}
+          {screenSize !== 'desktop' && (
             <>
-            <OpenLinksButton isMobileNavOpen={isMobileNavOpen}
-              onClick={() => {
-                setIsMobileNavOpen((current) => !current);
-              }}
-            >
-              {isMobileNavOpen ? <>&#10005;</> : <> &#8801;</>}
+              <OpenLinksButton
+                isMobileNavOpen={isMobileNavOpen}
+                onClick={() => setIsMobileNavOpen((current) => !current)}
+              >
+                {isMobileNavOpen ? <>&#10005;</> : <> &#8801;</>}
               </OpenLinksButton>
+              {isMobileNavOpen && (
+                <MobileContainer>
+                  {screenSize !== 'tablet' && <MobileAuth>{authNav}</MobileAuth>}
+                  {nav}
+                </MobileContainer>
+              )}
             </>
           )}
-          
-          {isMobileNavOpen && screenSize !== 'xl' &&(
-              <MobileContainer>
-              {screenSize !== 'lg' &&
-                (<MobileAuth>
-                <AuthNav />
-                {/* {!isLoggedIn ? <AuthNav /> : <UserNav />} */}
-              </MobileAuth>)}
-                <Nav />
-              </MobileContainer>)}
         </HeaderContainer>
       </Container>
     </AppHeader>
