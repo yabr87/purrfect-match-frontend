a3adf404ecf48fdf1a38db79a2abdca67ccf219d
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2023-05-13 02:06:53 +0300

    applied theme, upd. btns & instr-ns, start News

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import UserPage from 'pages/UserPage';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'utils/theme';
 import NoticesPage from '../../pages/NoticesPage/NoticesPage';
+import NewsPage from 'pages/NewsPage';
 import AddPetPage from 'pages/AddPetPage';
 
 // const HomePage = lazy(() => import('pages/HomePage'));z
@@ -17,7 +18,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="/notices/:categoryName" element={<NoticesPage/>} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/notices/:categoryName" element={<NoticesPage />} />
           <Route path="/add-pet" element={<AddPetPage />} />
           <Route path="/notices" element={<Navigate to="/notices/sell" />} />
         </Route>
