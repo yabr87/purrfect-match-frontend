617588365ea738ed39687bb7aa6704249db77f22
Maks Thimbarov <imaksim2@icloud.com>
2023-05-12 19:47:23 +0300

    add validation

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,9 @@
+import { ErrorMessage, Formik } from 'formik';
+import * as Yup from 'yup';
+
 import {
   FormContainer,
-  Form,
+  Forms,
   CenrteredDiv,
   StyledInput,
   InputContainer,
@@ -10,34 +13,55 @@ import {
 import { Btn } from 'shared/components/Button/Button.styled';
 import Title from 'shared/components/Title/Title';
 
+const SignupSchema = Yup.object().shape({
+  email: Yup.string().email('Enter a valid Email').required('Required'),
+  password: Yup.string()
+    .required('Required')
+    .min(6, 'Пароль повинен містити мінімум 6 символів')
+    .max(16, 'Пароль не може містити більше 16 символів')
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,16}$/,
+      'Пароль повинен містити мінімум 1 літеру верхнього регістру, 1 літеру нижнього регістру та 1 цифру'
+    ),
+});
 const LoginPage = () => {
   return (
     <CenrteredDiv>
       <FormContainer>
-        <Form>
-          <Title deskWeight={500} deskSize="36px">
-            Login
-          </Title>
-          <InputContainer>
-            <label>
-              <StyledInput type="text" name="email" placeholder="Email" />
-            </label>
-            <label>
-              <StyledInput
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
-            </label>
-          </InputContainer>
-          <Btn shape={'solid'} w={458} h={48}>
-            Login
-          </Btn>
-          <Text>
-            Don't have an account?{' '}
-            <StyledLink to={'/register'}>Register</StyledLink>
-          </Text>
-        </Form>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={SignupSchema}
+          onSubmit={values => {
+            console.log(values);
+          }}
+        >
+          <Forms>
+            <Title deskWeight={500} deskSize="36px">
+              Login
+            </Title>
+            <InputContainer>
+              <label>
+                <StyledInput type="text" name="email" placeholder="Email" />
+                <ErrorMessage name="email" />
+              </label>
+              <label>
+                <StyledInput
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
+                <ErrorMessage name="password" />
+              </label>
+            </InputContainer>
+            <Btn type="submit" shape={'solid'} w={458} h={48}>
+              Login
+            </Btn>
+            <Text>
+              Don't have an account?
+              <StyledLink to={'/register'}>Register</StyledLink>
+            </Text>
+          </Forms>
+        </Formik>
       </FormContainer>
     </CenrteredDiv>
   );
--- src/pages/LoginPage/LoginPage.styles.js
+++ src/pages/LoginPage/LoginPage.styles.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { Field, Form } from 'formik';
+
 export const FormContainer = styled.div`
   width: 608px;
   background: #ffffff;
   box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
   border-radius: 40px;
 `;
-export const Form = styled.form`
+export const Forms = styled(Form)`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -19,11 +21,20 @@ export const CenrteredDiv = styled.div`
 
   height: 100%;
 `;
-export const StyledInput = styled.input`
+export const StyledInput = styled(Field)`
   width: 100%;
   padding: 12px 16px;
   border: 1px solid #54adff;
   border-radius: 40px;
+
+  ::placeholder {
+    font-weight: 400;
+    font-size: 16px;
+    color: #888888;
+  }
+  :focus {
+    outline: none;
+  }
 `;
 export const InputContainer = styled.div`
   display: flex;
