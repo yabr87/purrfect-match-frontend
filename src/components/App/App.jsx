e448623646fd981be4f5abc08203d91852dfb5ff
Maks Thimbarov <imaksim2@icloud.com>
2023-05-12 17:10:08 +0300

    add base

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from 'layouts/SharedLayout';
 import HomePage from 'pages/HomePage';
 import NoticesPage from '../../pages/NoticesPage/NoticesPage';
 
-// const HomePage = lazy(() => import('pages/HomePage'));z
+// const HomePage = lazy(() => import('pages/HomePage'));
 // const RegisterPage = lazy(() => import('pages/RegisterPage'));
-// const LoginPage = lazy(() => import('pages/LoginPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/notices/:categoryName" element={<NoticesPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Route>
       {/* <Route path="*" element={<ErrorPage />} /> */}
     </Routes>
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,46 @@
-import { Test } from './LoginPage.styles';
+import {
+  FormContainer,
+  Form,
+  CenrteredDiv,
+  StyledInput,
+  InputContainer,
+  Text,
+  StyledLink,
+} from './LoginPage.styles';
+import { Btn } from 'shared/components/Button/Button.styled';
+import Title from 'shared/components/Title/Title';
 
-const LoginPage = () => {};
+const LoginPage = () => {
+  return (
+    <CenrteredDiv>
+      <FormContainer>
+        <Form>
+          <Title deskWeight={500} deskSize="36px">
+            Login
+          </Title>
+          <InputContainer>
+            <label>
+              <StyledInput type="text" name="email" placeholder="Email" />
+            </label>
+            <label>
+              <StyledInput
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+            </label>
+          </InputContainer>
+          <Btn shape={'solid'} w={458} h={48}>
+            Login
+          </Btn>
+          <Text>
+            Don't have an account?{' '}
+            <StyledLink to={'/register'}>Register</StyledLink>
+          </Text>
+        </Form>
+      </FormContainer>
+    </CenrteredDiv>
+  );
+};
 
 export default LoginPage;
