17469c390400095e1e51f4540f584edd9641ccac
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2023-05-12 16:08:11 +0300

    add png

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,10 +2,10 @@ import { Title, Pictures } from './MainPage.styles';
 
 const MainPage = () => {
   return (
-    <>
+    <div style={{ position: 'relative' }}>
       <Title>Take good care of your small pets</Title>
-      <Pictures></Pictures>
-    </>
+      <Pictures />
+    </div>
   );
 };
 
--- src/pages/MainPage/MainPage.styles.js
+++ src/pages/MainPage/MainPage.styles.js
@@ -1,4 +1,10 @@
 import styled from 'styled-components';
+import mobPet1 from '../../images/mainPage/pets@1x-mob.png';
+import mobPet2 from '../../images/mainPage/pets@2x-mob.png';
+import tabPet1 from '../../images/mainPage/pets@1x-tab.png';
+import tabPet2 from '../../images/mainPage/pets@2x-tab.png';
+import deskPet1 from '../../images/mainPage/pets@1x-desk.png';
+import deskPet2 from '../../images/mainPage/pets@2x-desk.png';
 
 export const Title = styled.h1`
   margin-top: 60px;
@@ -10,11 +16,38 @@ export const Title = styled.h1`
 `;
 export const Pictures = styled.div`
   margin-top: 20px;
-  ${'' /* background-repeat: no-repeat; */}
-  background-position: center;
-  background-color: #000000;
-  width: 474px;
+  background-repeat: no-repeat;
+  position: absolute;
+  background-image: url(${mobPet1});
+  background-size: 320px 394px;
+  width: 320px;
   height: 394px;
-  background-image: url('../../images/mainPage/pictures-1x-mob.jpg');
-  background-size: 474px 394px;
+
+  @media (min-device-pixel-ratio: 2),
+    (min-resolution: 192dpi),
+    (min-resolution: 2dppx) {
+    background-image: url(${mobPet2});
+  }
+
+  @media screen and (min-width: 768px) {
+    background-size: cover;
+    background-image: url(${tabPet1});
+
+    @media screen and (min-width: 768px) and (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${tabPet2});
+    }
+  }
+
+  @media screen and (min-width: 1280px) {
+    background-size: cover;
+    background-image: url(${deskPet1});
+
+    @media screen and (min-width: 1280px) and (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${deskPet2});
+    }
+  }
 `;
