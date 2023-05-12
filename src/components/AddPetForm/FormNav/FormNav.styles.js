03c006a7e21eebacbd7097fa5832609adf268e93
Anna Bramarova <annabramarova@gmail.com>
2023-05-12 20:14:07 +0300

    fix

--- src/components/AddPetForm/FormNav/FormNav.styles.js
+++ src/components/AddPetForm/FormNav/FormNav.styles.js
@@ -11,11 +11,10 @@ export const Step = styled.p`
   line-height: 14px;
     color: ${(props) => {
         if (props.active) {
-            return '#54ADFF'; // якщо компонент активний, повертаємо колір #54ADFF
+            return '#54ADFF'; 
         } else if (props.disabled) {
-            return '#888888'; // якщо компонент вимкнений, повертаємо колір #888888
+            return '#888888'; 
         } else {
-            return '#00C3AD'; // в іншому випадку повертаємо колір #00C3AD
+            return '#00C3AD';
         }
     }}`;
-    
