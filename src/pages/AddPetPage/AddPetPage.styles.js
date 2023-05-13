d0eb8207ec20159d7285ab09b3aca9241d265c70
Anna Bramarova <annabramarova@gmail.com>
2023-05-13 16:14:39 +0300

    add: notice/sell fix and adaptive for form

--- src/pages/AddPetPage/AddPetPage.styles.js
+++ src/pages/AddPetPage/AddPetPage.styles.js
@@ -1,12 +1,8 @@
 import styled from 'styled-components';
 
 export const AddPetWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
   padding-top: 24px;
 
-  
   @media screen and (min-width: 768px) {
   padding-top: 70px;
   }
