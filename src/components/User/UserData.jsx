9c2a99cb2895c452a6c1837659d9ca0c404dd392
Ihor <108004180+beligrous@users.noreply.github.com>
2023-05-15 23:39:41 +0300

    finish desktop markup

--- src/components/User/UserData.jsx
+++ src/components/User/UserData.jsx
@@ -9,33 +9,36 @@ import {
   LogOutText,
   InputItem,
   InputContainer,
+  Wrap,
 } from './';
 import Icon from 'shared/components/Icon/Icon';
 
 const UserData = () => {
   return (
     <>
-      <Title>My information:</Title>
-      <Container>
-        <AvatarContainer>
-          <Avatar />
-          <EditAvatarBtn>
-            <Icon id="camera" s={props => props.theme.colors.link} />
-            <BtnText>Edit photo</BtnText>
-          </EditAvatarBtn>
-        </AvatarContainer>
-        <InputContainer>
-          <InputItem name={'Name'} />
-          <InputItem name={'Email'} />
-          <InputItem name={'Birthday'} />
-          <InputItem name={'Phone'} />
-          <InputItem name={'City'} />
-          <LogOutBtn>
-            <Icon id="logout" s={props => props.theme.colors.link} />
-            <LogOutText>Log Out</LogOutText>
-          </LogOutBtn>
-        </InputContainer>
-      </Container>
+      <Wrap>
+        <Title>My information:</Title>
+        <Container>
+          <AvatarContainer>
+            <Avatar />
+            <EditAvatarBtn>
+              <Icon id="camera" s={props => props.theme.colors.link} />
+              <BtnText>Edit photo</BtnText>
+            </EditAvatarBtn>
+          </AvatarContainer>
+          <InputContainer>
+            <InputItem name={'Name'} />
+            <InputItem name={'Email'} />
+            <InputItem name={'Birthday'} />
+            <InputItem name={'Phone'} />
+            <InputItem name={'City'} />
+            <LogOutBtn>
+              <Icon id="logout" s={props => props.theme.colors.link} />
+              <LogOutText>Log Out</LogOutText>
+            </LogOutBtn>
+          </InputContainer>
+        </Container>
+      </Wrap>
     </>
   );
 };
