d48deaaaac41369905fb55322ca0afc34b2cf7c7
Anna Bramarova <annabramarova@gmail.com>
2023-05-15 15:51:00 +0300

    update: validation patterns

--- src/components/AddPetForm/validationSchema.js
+++ src/components/AddPetForm/validationSchema.js
@@ -10,13 +10,17 @@ const validationSchema = (selectedOption, currentStep) => {
           /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
           'Title may contain only letters, apostrophes, dashes, and spaces. For example: Adorable Puppies for Adoption'
         )
-        .required('Title is required'),
+        .required('Title is required')
+        .min(2, 'Name must be at least 2 characters')
+        .max(20, 'Name must not exceed 50 characters'),
       name: Yup.string()
         .required('Name is required')
         .matches(
           /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
           'Name may contain only letters, apostrophe, dash, and spaces. For example: Bobby, Buddy'
-        ),
+        )
+        .min(2, 'Name must be at least 2 characters')
+        .max(30, 'Name must not exceed 50 characters'),
       birthday: Yup.string()
         .required('Birthday is required')
         .matches(
@@ -28,15 +32,19 @@ const validationSchema = (selectedOption, currentStep) => {
         .matches(
           /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
           "Please enter your pet's breed using only letters, hyphens, apostrophes, commas, periods, and spaces. (For example: Golden Retriever, Siamese, Bulldog)"
-        ),
+        )
+        .min(2, 'Name must be at least 2 characters')
+        .max(50, 'Name must not exceed 50 characters'),
     });
   }
 
   if (currentStep === 3) {
     schema = schema.shape({
-      photo: Yup.string().required('Photo is required'),
-      comments: Yup.string().required('Comments is required'),
-      sex: Yup.string().required('Sex is required'),
+      photo: Yup.string().required('To finish form, please upload foto'),
+      comments: Yup.string().required('Comments is required')
+        .min(10, 'Comments must be at least 10 characters')
+        .max(200, 'Comments must not exceed 200 characters'),
+      sex: Yup.string().required('Sex is required').oneOf(['male', 'female']),
       location: Yup.string().required('Location is required'),
       price: Yup.number()
         .typeError('Price must be a number')
