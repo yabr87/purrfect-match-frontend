86a7dc784ffa250f3cd34fdec064c8418431012d
Ihor <108004180+beligrous@users.noreply.github.com>
2023-05-15 21:53:54 +0300

    add User index

---
+++ src/components/User/index.js
@@ -0,0 +1,30 @@
+import UserData from './UserData';
+import PetsData from './PetsData';
+import UserDataItem from './UserDataItem';
+import * as Styled from './UserData.styled';
+import * as ItemStyled from './UserDataItem.styled';
+import * as PetStyled from './PetsData.styled';
+
+export const Container = Styled.Container;
+export const PetContainer = PetStyled.PetContainer;
+export const Title = Styled.Title;
+export const PetHeader = PetStyled.PetHeader;
+export const PetInfoWrap = PetStyled.PetInfoWrap;
+export const PetInfoItem = PetStyled.PetInfoItem;
+export const PetInfoTitle = PetStyled.PetInfoTitle;
+export const DelPetBtn = PetStyled.DelPetBtn;
+export const Avatar = Styled.Avatar;
+export const AvatarContainer = Styled.AvatarContainer;
+export const PetAvatar = PetStyled.PetAvatar;
+export const EditAvatarBtn = Styled.EditAvatarBtn;
+export const LogOutBtn = Styled.LogOutBtn;
+export const BtnText = Styled.BtnText;
+export const LogOutText = Styled.LogOutText;
+export const InputContainer = Styled.InputContainer;
+export const Input = ItemStyled.Input;
+export const EditInputBtn = ItemStyled.EditInputBtn;
+export const ItemContainer = Styled.ItemContainer;
+
+export const InputItem = UserDataItem;
+export const PetsInfo = PetsData;
+export const UserInfo = UserData;
