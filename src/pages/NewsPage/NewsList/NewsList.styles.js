2d70f2fd6d74a3d57192472547a5104b07f9640b
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2023-05-16 22:55:01 +0300

    some fix

--- src/pages/NewsPage/NewsList/NewsList.styles.js
+++ src/pages/NewsPage/NewsList/NewsList.styles.js
@@ -5,15 +5,11 @@ export const List = styled.ul`
   flex-wrap: wrap;
   justify-content: center;
   row-gap: 44px;
-  margin-top: 44px;
+  margin-top: 20px;
+  column-gap: 32px;
 
   @media ${({ theme }) => theme.media.tab} {
     row-gap: 46px;
-    column-gap: 32px;
-    margin-top: 62px;
-  }
-
-  @media ${({ theme }) => theme.media.desk} {
-    margin-top: 82px;
+    margin-top: 26px;
   }
 `;
--- src/shared/components/Section/Section.jsx
+++ src/shared/components/Section/Section.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Wrapper } from './Section.styles';
 
-const Section = ({ children }) => <Wrapper>{children}</Wrapper>;
+const Section = ({ children, ...props }) => (
+  <Wrapper {...props}>{children}</Wrapper>
+);
 
 Section.propTypes = {
   children: PropTypes.node.isRequired,
