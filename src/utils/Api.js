83fb6456f4b23f00bd329d5f611666c0ac1370e8
ViktorZvonar <viktorzvonar@yahoo.com>
2023-05-16 17:59:13 +0300

    deleteApi, favoriteApi done

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,18 +1,27 @@
 import axios from 'axios';
 
-const instance = axios.create({
+export const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
+
+//добавив тут
+export const setToken = token => {
+  instance.defaults.headers.authorization = token ? `Bearer ${token}` : '';
+};
 
 // const setToken = token => {};
 
 export const signup = async data => {
   const response = await instance.post('/api/users/register', data);
+  //добавив тут
+  setToken(response.data.token);
   return response;
 };
 
 export const login = async data => {
   const response = await instance.post('/api/users/login', data);
+  //добавив тут
+  setToken(response.data.token);
   return response;
 };
 
