f4af62691373bf2b3e78821dc5ea7747ea4e27c7
yabr87 <108272773+yabr87@users.noreply.github.com>
2023-05-16 23:44:06 +0300

    small fix added padding

--- src/pages/NoticesPage/NoticesSearch/NoticesSearch.jsx
+++ src/pages/NoticesPage/NoticesSearch/NoticesSearch.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import Section from 'shared/components/Section';
 import Search from 'shared/components/Search/Search';
 import Title from 'shared/components/Title';
-import { Cover } from './NoticesSearch.styles';
+// import { Cover } from './NoticesSearch.styles';
 
 const NoticesSearch = () => {
   const handleSubmit = (values, action) => {
@@ -22,10 +22,10 @@ const NoticesSearch = () => {
     // };
   };
   return (
-    <Cover>
+    <>
       <Title>Find your favorite pet</Title>
       <Search initValue={{ search: '' }} handleSubmit={handleSubmit} />
-    </Cover>
+    </>
   );
 };
 
