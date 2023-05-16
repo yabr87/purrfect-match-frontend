e8a36d1de1bc2c18aff1f79b8e3d8408522d4b28
Ihor <108004180+beligrous@users.noreply.github.com>
2023-05-16 22:01:12 +0300

    add logout !

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -4,20 +4,28 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-// const setToken = token => {};
+const setToken = token => {
+  if (token) {
+    return (instance.defaults.headers.authorization = `Bearer ${token}`);
+  }
+  instance.defaults.headers.authorization = '';
+};
 
 export const signup = async data => {
   const response = await instance.post('/api/users/register', data);
+  setToken(response.data.token);
   return response;
 };
 
 export const login = async data => {
   const response = await instance.post('/api/users/login', data);
+  setToken(response.data.token);
   return response;
 };
 
 export const logout = async () => {
   await instance.post('api/users/logout');
+  setToken();
 };
 
 export const getCurrent = async token => {};
