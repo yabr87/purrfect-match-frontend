b699fe9bded741bf27fc6c169fd7244e14c2c165
Ihor <108004180+beligrous@users.noreply.github.com>
2023-05-17 23:39:40 +0300

    add user input handler/ to fix

--- src/components/User/UserDataItem.jsx
+++ src/components/User/UserDataItem.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Input, ItemContainer, EditInputBtn } from './';
 import Icon from 'shared/components/Icon/Icon';
+import { useSelector } from 'react-redux';
+import { updateUserInfo } from 'utils/Api';
 
 const UserDataItem = ({ name, type, pattern, value }) => {
   const [data, setData] = useState(value);
   const [disable, setDisable] = useState(true);
+  const token = useSelector(store => store.auth.token);
 
   const handleInputEdit = () => {
     setDisable(false);
+  };
+
+  const handleInputSubmit = async () => {
+    await updateUserInfo(token, { name: data });
   };
 
   return (
@@ -21,14 +28,17 @@ const UserDataItem = ({ name, type, pattern, value }) => {
         pattern={pattern}
         name={name}
         disabled={disable}
+        autoFocus={!disable}
       />
-      <EditInputBtn onClick={handleInputEdit}>
-        {disable ? (
+      {disable ? (
+        <EditInputBtn onClick={handleInputEdit}>
           <Icon id="edit" s="#54ADFF" />
-        ) : (
+        </EditInputBtn>
+      ) : (
+        <EditInputBtn onClick={handleInputSubmit}>
           <Icon id="check" s="#00C3AD" />
-        )}
-      </EditInputBtn>
+        </EditInputBtn>
+      )}
     </ItemContainer>
   );
 };
