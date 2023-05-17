ce3d9e46ee9f5e27c7d9ef77a50c71cdc580f9e2
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2023-05-18 00:35:06 +0300

    connect backend

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,65 +1,14 @@
-// import { useState, useEffect, useCallback } from 'react';
-
 import Container from 'shared/components/Container';
 import Title from 'shared/components/Title';
 import Search from 'shared/components/Search';
-import NewsList from './NewsList';
-// import Button from 'shared/components/Button';
-
-// import { getAllNews } from 'utils/news-api';
-
-import items from './items.json';
+import News from './News';
 
 const NewsPage = () => {
-  // const [items, setItems] = useState([]);
-  // const [query, setQuery] = useState('');
-  // const [page, setPage] = useState(1);
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState(null);
-
-  // const onFormSubmit = useCallback(
-  //   search => {
-  //     if (search === query) return;
-  //     setQuery(search);
-  //     setPage(1);
-  //     setItems([]);
-  //     setError(null);
-  //   },
-  //   [query]
-  // );
-
-  // useEffect(() => {
-  //   const fetchAllNews = async () => {
-  //     try {
-  //       setLoading(true);
-  //       const { hits } = await getAllNews(query, page);
-  //       // setItems([...hits]);
-  //       console.log('hits:', hits);
-  //       setItems(prevItems => [...prevItems, ...hits]);
-  //     } catch (error) {
-  //       setError(error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-  //   fetchAllNews();
-  // }, [page]);
-
-  // const loadMore = useCallback(() => {
-  //   setPage(prevPage => prevPage + 1);
-  // }, []);
-
   return (
     <Container>
       <Title>News</Title>
       <Search />
-
-      {/* {error && <p>{error.message}</p>} */}
-
-      {/* {loading && <div>Loading...</div>} */}
-
-      {Boolean(items.length) && <NewsList items={items} />}
-      {/* <Button onClick={loadMore}>Load more</Button> */}
+      <News />
     </Container>
   );
 };
