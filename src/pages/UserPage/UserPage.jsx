e8e157e49cf0befa0286885b427c646c97ec7721
Mary Danyliuk <marydanyliuk@gmail.com>
2023-05-17 20:06:54 +0300

    add Congrats Modal to Registration

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,14 +1,38 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import { User } from './';
 import { UserInfo, PetsInfo } from 'components/User';
 import Container from 'shared/components/Container';
 
+import ModalApproveAction from 'components/ModalApproveAction';
+import Congrats from 'components/ModalApproveAction/Congrats';
+
 const UserPage = () => {
+  const location = useLocation();
+
+  const [isOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const state = location.state;
+    // console.log(state);
+    if (state) {
+      console.log(state);
+      const { params } = location.state;
+      setIsModalOpen(params);
+    }
+  }, [location.state]);
   return (
     <Container>
       <User>
         <UserInfo />
         <PetsInfo />
       </User>
+      {isOpen && (
+        <ModalApproveAction close={() => setIsModalOpen(false)}>
+          <Congrats close={() => setIsModalOpen(false)} />
+        </ModalApproveAction>
+      )}
     </Container>
   );
 };
