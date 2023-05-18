25be1269fd733042fdf7cbc981a0ecca8cad8d5b
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-05-18 12:44:29 +0200

    OurFriendsPage update
    Deleted margins

--- src/pages/OurFriendsPage/OurFriendsPage.styles.js
+++ src/pages/OurFriendsPage/OurFriendsPage.styles.js
@@ -1,15 +1,11 @@
 import styled from 'styled-components';
 
 export const FriendsContainer = styled.div`
-  margin-top: 40px;
-  margin-bottom: 40px;
   display: flex;
   flex-direction: column;
   align-items: center;
 
   @media screen and (min-width: 768px) {
-    margin-top: 80px;
-    margin-bottom: 50px;
   }
 
   @media screen and (min-width: 1280px) {
