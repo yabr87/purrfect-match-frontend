b08372e0f55cb195e05804d5efe1122b8e86b84e
Serhei Honcharov <mrsedmor@gmail.com>
2023-05-19 01:37:24 +0300

    fix/redux-and-persist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from 'utils/theme';
@@ -8,10 +8,13 @@ import { useEffect, useState } from 'react';
 import { refresh } from 'redux/auth/authOperations';
 import Switcher from 'components/themeSwitcher/Switcher';
 
+import RestrictedRoute from 'routes/RestrictedRoute';
+import PrivateRoute from 'routes/PrivateRoute';
 import SharedLayout from 'layouts/SharedLayout';
+import { useRef } from 'react';
 
 const MainPage = lazy(() => import('pages/MainPage'));
-const NoticesPage = lazy(() => import('../../pages/NoticesPage/NoticesPage'));
+const NoticesPage = lazy(() => import('pages/NoticesPage/NoticesPage'));
 const AddPetPage = lazy(() => import('pages/AddPetPage'));
 const ErrorPage = lazy(() => import('pages/ErrorPage'));
 const RegisterPage = lazy(() => import('pages/RegisterPage'));
@@ -19,15 +22,16 @@ const LoginPage = lazy(() => import('pages/LoginPage'));
 const UserPage = lazy(() => import('pages/UserPage'));
 const NewsPage = lazy(() => import('pages/NewsPage'));
 const OurFriendsPage = lazy(() => import('pages/OurFriendsPage'));
-const RestrictedRoute = lazy(() => import('routes/RestrictedRoute'));
-const PrivateRoute = lazy(() => import('routes/PrivateRoute'));
 
 const App = () => {
   const [currentTheme, setCurrentTheme] = useState(lightTheme);
   const dispatch = useDispatch();
-
+  const runOnce = useRef(true);
   useEffect(() => {
-    dispatch(refresh());
+    if (runOnce.current) {
+      runOnce.current = false;
+      dispatch(refresh());
+    }
   }, [dispatch]);
   const onClick = () => {
     setCurrentTheme(currentTheme === lightTheme ? darkTheme : lightTheme);
@@ -36,43 +40,45 @@ const App = () => {
   return (
     <ThemeProvider theme={currentTheme}>
       <Switcher onClick={onClick} />
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<MainPage />} />
-          <Route path="/notices/:categoryName" element={<NoticesPage />} />
-          <Route
-            path="/add-pet"
-            element={
-              <PrivateRoute redirectTo="/login" component={<AddPetPage />} />
-            }
-          />
-          <Route path="/notices" element={<Navigate to="/notices/sell" />} />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                redirectTo="/user"
-                component={<RegisterPage />}
-              />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute redirectTo="/user" component={<LoginPage />} />
-            }
-          />
-          <Route
-            path="/user"
-            element={
-              <PrivateRoute redirectTo="/login" component={<UserPage />} />
-            }
-          />
-          <Route path="/news" element={<NewsPage />} />
-          <Route path="/friends" element={<OurFriendsPage />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<MainPage />} />
+            <Route path="/notices/:categoryName" element={<NoticesPage />} />
+            <Route
+              path="/add-pet"
+              element={
+                <PrivateRoute redirectTo="/login" component={<AddPetPage />} />
+              }
+            />
+            <Route path="/notices" element={<Navigate to="/notices/sell" />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/user"
+                  component={<RegisterPage />}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute redirectTo="/user" component={<LoginPage />} />
+              }
+            />
+            <Route
+              path="/user"
+              element={
+                <PrivateRoute redirectTo="/login" component={<UserPage />} />
+              }
+            />
+            <Route path="/news" element={<NewsPage />} />
+            <Route path="/friends" element={<OurFriendsPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </ThemeProvider>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,22 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App/App';
 import { Provider } from 'react-redux/es/exports';
-import {
-  store,
-  // persistor
-} from './redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //  <React.StrictMode>
-  <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <BrowserRouter basename="purrfect-match-frontend">
-      <App />
-    </BrowserRouter>
-    {/* </PersistGate> */}
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="purrfect-match-frontend">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
