45a4e735f2eeb80c1e6b83261f4e1eefb2b9b374
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-05-19 11:18:35 +0200

    LanguageSwitcher
    LanguageSwitcher added (component, inserted in header)

---
+++ src/components/LanguageSwitcher/LanguageSwither.styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const LanguageSelectorContainer = styled.div``;
+
+export const Select = styled.select`
+  display: block;
+  border:none;
+  background:transparent;
+
+  font-weight: ${props => props.theme.fontWeiths.semi};
+  font-size: 32px;
+  line-height: 1.37;
+  letter-spacing: 0.04em;
+
+  color: ${props => props.theme.colors.black};
+  outline: none;
+  transition: color 250ms ease;
+
+  &:hover,
+  &:focus {
+    color: ${props => props.theme.colors.yellow};
+    font-weight: 700;
+  }
+
+  @media ${props => props.theme.media.tab} {
+    font-size: ${props => props.theme.fontSizes.title};
+
+    &:hover,
+    &:focus {
+      font-weight: ${props => props.theme.fontWeiths.semi};
+    }
+
+    @media ${props => props.theme.media.desk} {
+      padding: 8px 0;
+      font-size: ${props => props.theme.fontSizes.m};
+      line-height: 1.35;
+    }
+`;
---
+++ src/utils/languages/i18n.js
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: {
+        Our_friends: 'Our friends',
+      },
+    },
+    ukr: {
+      translation: {
+        Our_friends: 'Наші друзі',
+      },
+    },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
