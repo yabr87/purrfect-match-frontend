9bc6df4b4ca555e595d0adad071e37e9f5f4374c
ViktorZvonar <viktorzvonar@yahoo.com>
2023-05-19 18:29:15 +0300

    Update NoticesFilters.styles.js

--- src/pages/NoticesPage/NoticesFilters/NoticesFilters.styles.js
+++ src/pages/NoticesPage/NoticesFilters/NoticesFilters.styles.js
@@ -13,7 +13,7 @@ export const FilterOptions = styled.div`
   width: 152px;
   background: ${props => props.theme.colors.backgroundModal};
   box-shadow: ${props => props.theme.shadows.default};
-  border-radius: ${props => props.theme.radius.normal};
+  border-radius: ${props => props.theme.radius.small};
 `;
 
 export const FiltersTitle = styled.h3`
@@ -37,7 +37,7 @@ export const FiltersItems = styled.div`
   align-items: center;
   max-height: 32px;
   background: ${props => props.theme.colors.lightBlue};
-  border-radius: ${props => props.theme.radius.normal};
+  border-radius: ${props => props.theme.radius.small};
   font-style: normal;
   font-weight: ${props => props.theme.fontWeiths.normal};
   font-size: ${props => props.theme.fontSizes.s};
@@ -51,7 +51,7 @@ export const ContainerItem = styled.div`
   gap: 10px;
   background: ${props => props.theme.colors.lightBlue};
   color: ${props => props.theme.colors.link};
-  border-radius: ${props => props.theme.radius.normal};
+  border-radius: ${props => props.theme.radius.small};
   align-items: center;
   &:first-child {
     margin-bottom: 8px;
