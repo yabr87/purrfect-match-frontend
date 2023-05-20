80c52299f3473ae1636dfb58c2de3811da39ed38
Anna Bramarova <annabramarova@gmail.com>
2023-05-21 00:54:36 +0300

    add: disabled button

--- src/components/ModalApproveAction/EditModal/EditModal.jsx
+++ src/components/ModalApproveAction/EditModal/EditModal.jsx
@@ -1,9 +1,7 @@
 import { Form, Formik } from 'formik';
+import { useState } from 'react';
 import Loader from 'shared/components/Loader';
-import {
-  Error,
-  FormButton
-} from 'components/AddPetForm/AddPetForm.styles';
+import { Error, FormButton } from 'components/AddPetForm/AddPetForm.styles';
 import { reverseISODate } from 'utils/reverseISODate';
 import {
   AdaptiveBoxOne,
@@ -26,11 +24,10 @@ import validationSchemaEdit from './validationSchemaEdit';
 import { PetImage } from '../NoticeModal/NoticeModal.styles';
 import { editNotice } from 'utils/ApiNotices';
 import { convertToISODate } from 'utils/convertToISODate';
-
-
+import { toast } from 'react-toastify';
 
 const EditModal = ({ notice, close, approve }) => {
-
+  const [isFormDirty, setIsFormDirty] = useState(false);
   const navigate = useNavigate();
 
   const initialValues = {
@@ -47,204 +44,221 @@ const EditModal = ({ notice, close, approve }) => {
     promo: 0,
   };
 
-  const changedFields = {};
-const handleSubmit = async (values, { resetForm }) => {
-  Object.keys(values).forEach((key) => {
-    const initialValue = initialValues[key];
-    const currentValue = values[key];
+  const handleSubmit = async (values, { resetForm }) => {
+    const changedFields = {};
 
-    if (initialValue !== currentValue) {
-      changedFields[key] = currentValue;
+    Object.keys(values).forEach(key => {
+      const initialValue = initialValues[key];
+      const currentValue = values[key];
+
+      if (initialValue !== currentValue) {
+        changedFields[key] = currentValue;
+      }
+    });
+
+    if (Object.keys(changedFields).length === 0) {
+      return;
     }
-  });
 
-  const newPet = Object.keys(changedFields).reduce((acc, key) => {
-    if (key === 'category' && changedFields[key] === 'my-pet') {
+    const newPet = Object.keys(changedFields).reduce((acc, key) => {
+      if (key === 'category' && changedFields[key] === 'my-pet') {
+        return acc;
+      }
+
+      if (key === 'birthday') {
+        acc[key] = convertToISODate(changedFields[key]);
+      } else {
+        acc[key] = changedFields[key];
+      }
+
       return acc;
+    }, {});
+
+    try {
+      await editNotice(notice._id, newPet);
+      toast.success(`Pet edited successfully`);
+      resetForm();
+      navigate(`/notices/${notice.category}`);
+    } catch (error) {
+      toast.error(`Failed to edit pet':${error}`);
     }
+  };
 
-    if (key === 'birthday') {
-      acc[key] = convertToISODate(changedFields[key]);
-    } else {
-      acc[key] = changedFields[key];
-    }
-
-    return acc;
-  }, {});
-
-  try {
-    console.log(newPet);
-    await editNotice(notice._id, newPet);
-    resetForm();
-    navigate('/notices');
-  } catch (error) {
-    console.error('Failed to edit pet', error);
-  }
-};
+  const handleFormChange = () => {
+    setIsFormDirty(true);
+  };
 
   return (
     <EditContainer>
       <EditContent>
-                  <Formik onSubmit={handleSubmit} initialValues={initialValues} validationSchema={validationSchemaEdit(notice.category)}>
-            {({
-              isSubmitting,
-              handleChange,
-              handleBlur,
-              values,
-              errors,
-              touched,
-            }) => (
-              <Form>
-                {isSubmitting ? (
-                  <Loader />
-                ) : (
-                    <EditWrapper>
-                      <AdaptiveBoxOne>
-                      <EditTitle>Edit your pet</EditTitle>
-                      <div style={{position: 'relative', width: '100%', }}>
-                      <PetImage src={notice.photoUrl} alt={notice.title} style={ {margin: 0}}  />
+        <Formik
+          onSubmit={handleSubmit}
+          initialValues={initialValues}
+          validationSchema={validationSchemaEdit(notice.category)}
+        >
+          {({
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            values,
+            errors,
+            touched,
+          }) => (
+            <Form onChange={handleFormChange}>
+              {isSubmitting ? (
+                <Loader />
+              ) : (
+                <EditWrapper>
+                  <AdaptiveBoxOne>
+                    <EditTitle>Edit your pet</EditTitle>
+                    <div style={{ position: 'relative', width: '100%' }}>
+                      <PetImage
+                        src={notice.photoUrl}
+                        alt={notice.title}
+                        style={{ margin: 0 }}
+                      />
                       <ImageCategoryEdit>
                         {notice.category
                           .replace('for-free', 'for free')
                           .replace(/-/g, '/')}
-                      </ImageCategoryEdit></div>
-                        <EditLabel htmlFor="promo">
-                          {values.promo ? (
-                            <p>Raise your ad only for {values.promo}$!</p>
-                        ) : (
-                            <>
-                            <p>Add payment to raise your ad!</p>
-                              <p>Just scroll your value:</p>
-                            </>
-                          )}
+                      </ImageCategoryEdit>
+                    </div>
+                    <EditLabel htmlFor="promo">
+                      {values.promo ? (
+                        <p>Raise your ad only for {values.promo}$!</p>
+                      ) : (
+                        <>
+                          <p>Add payment to raise your ad!</p>
+                          <p>Just scroll your value:</p>
+                        </>
+                      )}
+                      <EditField
+                        type="range"
+                        id="promo"
+                        name="promo"
+                        min="0"
+                        max="30"
+                        placeholder="Raise your ad"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.promo}
+                        errors={touched.promo && errors.promo}
+                      />
+                      <Error name="promo" component="p" />
+                    </EditLabel>
+                  </AdaptiveBoxOne>
+                  <AdaptiveBoxTwo>
+                    <EditLabel htmlFor="title">
+                      Title of ad:
+                      <EditField
+                        name="title"
+                        placeholder="Type title of publication"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.title}
+                        errors={touched.title && errors.title}
+                      />
+                      <Error name="title" component="p" />
+                    </EditLabel>
+                    <EditLabel htmlFor="name">
+                      Name of pet:
+                      <EditField
+                        name="name"
+                        placeholder="Type name of pet"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.name}
+                        errors={touched.name && errors.name}
+                      />
+                      <Error name="name" component="p" />
+                    </EditLabel>
+                    <EditLabel htmlFor="birthday">
+                      Date of birth:
+                      <EditField
+                        name="birthday"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.birthday}
+                        errors={touched.birthday && errors.birthday}
+                      />
+                      <Error name="birthday" component="p" />
+                    </EditLabel>
+                    <EditLabel htmlFor="breed">
+                      Breed:
+                      <EditField
+                        name="breed"
+                        placeholder="Type name of pet"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.breed}
+                        errors={touched.breed && errors.breed}
+                      />
+                      <Error name="breed" component="p" />
+                    </EditLabel>
+                    <EditLabel htmlFor="location">
+                      Location:
+                      <EditField
+                        name="location"
+                        placeholder="Type name of pet"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.location}
+                        errors={touched.location && errors.location}
+                      />
+                      <Error name="location" component="p" />
+                      {notice.category === 'sell' && (
+                        <EditLabel htmlFor="price">
+                          Type your new price:
                           <EditField
-                            type="range"
-                            id="promo"
-                            name="promo"
-                            min="0"
-                            max="30"
-                            placeholder="Raise your ad"
+                            name="price"
+                            placeholder="Type your new price"
                             onChange={handleChange}
                             onBlur={handleBlur}
-                            value={values.promo}
-                            errors={touched.promo && errors.promo}
+                            value={values.price}
+                            errors={touched.price && errors.price}
                           />
-                          <Error name="promo" component="p" />
+                          <Error name="price" component="p" />
                         </EditLabel>
-                    </AdaptiveBoxOne>
-                    <AdaptiveBoxTwo>
-                      <EditLabel htmlFor="title">
-                        Title of ad:
-                        <EditField
-                          name="title"
-                          placeholder="Type title of publication"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.title}
-                          errors={touched.title && errors.title}
-                        />
-                        <Error name="title" component="p" />
-                      </EditLabel>
-                      <EditLabel htmlFor="name">
-                        Name of pet:
-                        <EditField
-                          name="name"
-                          placeholder="Type name of pet"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.name}
-                          errors={touched.name && errors.name}
-                        />
-                        <Error name="name" component="p" />
-                      </EditLabel>
-                      <EditLabel htmlFor="birthday">
-                        Date of birth:
-                        <EditField
-                          name="birthday"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.birthday}
-                          errors={touched.birthday && errors.birthday}
-                        />
-                        <Error name="birthday" component="p" />
-                      </EditLabel>
-                      <EditLabel htmlFor="breed">
-                        Breed:
-                        <EditField
-                          name="breed"
-                          placeholder="Type name of pet"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.breed}
-                          errors={touched.breed && errors.breed}
-                        />
-                        <Error name="breed" component="p" />
-                      </EditLabel>
-                      <EditLabel htmlFor="location">
-                        Location:
-                        <EditField
-                          name="location"
-                          placeholder="Type name of pet"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.location}
-                          errors={touched.location && errors.location}
-                        />
-                        <Error name="location" component="p" />
-                        {notice.category === 'sell' && (
-                          <EditLabel htmlFor="price">
-                            Type your new price:
-                            <EditField
-                              name="price"
-                              placeholder="Type your new price"
-                              onChange={handleChange}
-                              onBlur={handleBlur}
-                              value={values.price}
-                              errors={touched.price && errors.price}
-                            />
-                            <Error name="price" component="p" />
-                          </EditLabel>
-                        )}
-                      </EditLabel>
-                      <EditLabel htmlFor="comments">
-                        Comments:
-                        <EditText
-                          as="textarea"
-                          name="comments"
-                          placeholder="Type name of pet"
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          value={values.comments}
-                          errors={touched.comments && errors.comments}
-                        />
-                        <Error name="comments" component="p" />
-                      </EditLabel>
-                    </AdaptiveBoxTwo>
-                  </EditWrapper>
-                )}
-                <EditBox>
-                  <FormButton type="button" w="248" h="48" onClick={close}>
-                    Cancel
-                  </FormButton>
-                  <Button
-                    type="submit"
-                    w="248"
-                    h="48"
-                    shape="solid"
-                    disabled={isSubmitting}
-                    onClick={approve}
-                  >
-                    Update
-                    <Icon id="paw" f="currentColor" s="none" />
-                  </Button>
-                </EditBox>
-              </Form>
-            )}
-          </Formik>
+                      )}
+                    </EditLabel>
+                    <EditLabel htmlFor="comments">
+                      Comments:
+                      <EditText
+                        as="textarea"
+                        name="comments"
+                        placeholder="Type name of pet"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.comments}
+                        errors={touched.comments && errors.comments}
+                      />
+                      <Error name="comments" component="p" />
+                    </EditLabel>
+                  </AdaptiveBoxTwo>
+                </EditWrapper>
+              )}
+              <EditBox>
+                <FormButton type="button" w="248" h="48" onClick={close}>
+                  Cancel
+                </FormButton>
+                <Button
+                  type="submit"
+                  w="248"
+                  h="48"
+                  shape="solid"
+                  disabled={isSubmitting || !isFormDirty}
+                  onClick={approve}
+                >
+                  Update
+                  <Icon id="paw" f="currentColor" s="none" />
+                </Button>
+              </EditBox>
+            </Form>
+          )}
+        </Formik>
       </EditContent>
     </EditContainer>
   );
 };
 
 export default EditModal;
-
