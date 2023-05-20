07922e9fe9b55dc4a7a9ca56668623469c70f6c8
NikolayDvoinikh <nikolaydvoinikh@gmail.com>
2023-05-20 13:19:10 +0300

    fixed title styles and search

--- src/shared/hooks/pagination/Pagination.styles.js
+++ src/shared/hooks/pagination/Pagination.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ButtonPage = styled.button`
   display: flex;
-  color: #54adff;
+  color: ${({ theme }) => theme.colors.link};
   align-items: center;
   justify-content: center;
   cursor: pointer;
@@ -10,16 +10,17 @@ export const ButtonPage = styled.button`
   height: 35px;
   border: 1px solid #54adff;
   border-radius: 50%;
-  font-family: 'Inter';
+  font-family: ${({ theme }) => theme.fonts.placeholders};
   font-style: normal;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 15px;
+  font-weight: ${({ theme }) => theme.fontWeiths.normal};
+  font-size: ${({ theme }) => theme.fontSizes.s};
+  line-height: 0.94;
+  background-color: transparent;
 
   &.current_page {
-    color: #fef9f9;
-    background-color: #54adff;
-    border-color: #54adff;
+    color: ${({ theme }) => theme.colors.backgroundColor};
+    background-color: ${({ theme }) => theme.colors.link};
+    border-color: ${({ theme }) => theme.colors.link};
   }
 `;
 
@@ -27,9 +28,10 @@ export const Flex = styled.div`
   padding: 8px 12px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  background-color: #fef9f9;
-
+  justify-content: center;
+  gap: 10px;
+  background-color: transparent;
+  outline: ${({ theme }) => theme.colors.pagginationOutline};
   box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
   border-radius: 45px;
 `;
@@ -37,4 +39,6 @@ export const Box = styled.div`
   margin: 60px auto 105px;
   width: 100%;
   max-width: 320px;
+  display: flex;
+  justify-content: center;
 `;
