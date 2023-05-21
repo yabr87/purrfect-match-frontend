17c56d5c5cb3933e60e691210ab92c5621fe8b12
Ihor <108004180+beligrous@users.noreply.github.com>
2023-05-21 13:42:09 +0300

    +

--- src/pages/UserPage/UserPage.styled.js
+++ src/pages/UserPage/UserPage.styled.js
@@ -1,13 +1,6 @@
 import styled from 'styled-components';
 
 export const User = styled.div`
-  padding-top: 40px;
-  padding-bottom: 137px;
-
-  @media screen and (min-width: 768px) {
-    padding-bottom: 121px;
-  }
-
   @media screen and (min-width: 1280px) {
     display: flex;
   }
