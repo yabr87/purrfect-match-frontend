3ea14abb45064732f3f916c12552df62f13aae62
NikolayDvoinikh <nikolaydvoinikh@gmail.com>
2023-05-21 13:53:29 +0300

    Styles is correct on ios. Move component Pagination from hooks to components

--- src/shared/components/Pagination/Pagination.styles.js
+++ src/shared/components/Pagination/Pagination.styles.js
@@ -2,19 +2,19 @@ import styled from 'styled-components';
 
 export const ButtonPage = styled.button`
   display: flex;
-  color: ${({ theme }) => theme.colors.link};
+  padding: 0;
   align-items: center;
   justify-content: center;
-  cursor: pointer;
   width: 35px;
   height: 35px;
   border: 1px solid #54adff;
+  color: ${({ theme }) => theme.colors.link};
   border-radius: 50%;
-  font-family: ${({ theme }) => theme.fonts.placeholders};
-  font-style: normal;
-  font-weight: ${({ theme }) => theme.fontWeiths.normal};
-  font-size: ${({ theme }) => theme.fontSizes.s};
+  cursor: pointer;
   line-height: 0.94;
+  font-size: ${({ theme }) => theme.fontSizes.s};
+  font-weight: ${({ theme }) => theme.fontWeiths.normal};
+  font-family: ${({ theme }) => theme.fonts.placeholders};
   background-color: transparent;
 
   &.current_page {
@@ -31,7 +31,7 @@ export const Flex = styled.div`
   justify-content: center;
   gap: 10px;
   background-color: transparent;
-  outline: ${({ theme }) => theme.colors.pagginationOutline};
+  border: ${({ theme }) => theme.colors.pagginationOutline};
   box-shadow: 3px 8px 14px rgba(136, 198, 253, 0.19);
   border-radius: 45px;
 `;
