b8d0e5bc88f6b5a28d218f2a4a9a814bece2605f
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-05-22 14:51:58 +0200

    NoticesPage-alert-lang

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -169,7 +169,7 @@ function NoticesPage() {
         <SelectedFilters
           filters={selectedFilters}
           onChange={() => {
-            toast.warning(t('You removed one of the filters'));
+            toast.warning(t('alert_You_removed_one_filters'));
           }}
         />
       </div>
