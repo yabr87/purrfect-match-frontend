f9ff7f124783bf22d52e10bd9a13c5354e5d8508
Anna Bramarova <annabramarova@gmail.com>
2023-05-22 17:14:33 +0300

    fix: navigate to user and notice page

--- src/components/Header/UserNav/UserNav.jsx
+++ src/components/Header/UserNav/UserNav.jsx
@@ -2,9 +2,17 @@ import PropTypes from 'prop-types';
 import Icon from 'shared/components/Icon/Icon';
 import { UserContainer, UserLink, UserName } from './UserNav.styles';
 import useAuth from 'shared/hooks/useAuth';
+import { useEffect } from 'react';
+
+
 
 const UserNav = ({ handleLinkClick }) => {
   const { user } = useAuth();
+
+  useEffect(() => {
+    
+  }, [user.name]);
+
   return (
     <UserContainer>
       <UserLink to="/user" onClick={handleLinkClick}>
